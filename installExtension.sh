f7f7a4e82877d5a560ff36fc91ff05c36a40e774
Stephen <stephen.fouse@gmail.com>
2017-01-05 15:13:25 -0500

    Fixed install script

--- installExtension.sh
+++ installExtension.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cp -r $PWD/* /root/.local/share/gnome-shell/extensions/topbarpinger@stephenfouse.gmail.com/
+mkdir -p /root/.local/share/gnome-shell/extensions/topbarpinger@stephenfouse.gmail.com/
+cp -rf $PWD/* /root/.local/share/gnome-shell/extensions/topbarpinger@stephenfouse.gmail.com/
 gnome-shell-extension-tool -e topbarpinger@stephenfouse.gmail.com
 echo "Now restart the Gnome-shell using 'ALT + F2' and type 'r'"
